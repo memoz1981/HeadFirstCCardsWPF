0a4ee1bacb92f634ef03b646053724c5c0da18d8
Mehdi Zeynalov <zeynalov@gmail.com>
2020-09-27 13:20:27 +0400

    Initial Project - Form Built and card class formed.

---
+++ Chp 7-2/Suits.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chp_8_1
+{
+    enum Suits
+    {
+        Spades, Clubs, Diamonds, Hearts
+    }
+}
